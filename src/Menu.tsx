8f1038d1e38b9e3bad105c3c89e237850ffee070
Cory House <housecor@gmail.com>
2023-04-03 15:31:35 -0500

    style menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -3,17 +3,18 @@ import { Food, foods } from "./foods";
 export function Menu() {
   function renderFood(food: Food) {
     return (
-      <li>
-        {food.name} - ${food.price}
-      </li>
+      <div className="max-w-sm p-4 m-2 transition-colors bg-blue-100 border border-gray-400 rounded-lg shadow-lg hover:bg-indigo-600 hover:text-white">
+        <h2 className="text-2xl font-bold">{food.name}</h2>
+        <p>{food.description}</p>
+        <strong>${food.price}</strong>
+      </div>
     );
   }
 
   return (
     <>
       <h1>Menu</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ul>{foods.map(renderFood)}</ul>
+      <div className="flex flex-wrap">{foods.map(renderFood)}</div>
     </>
   );
 }
