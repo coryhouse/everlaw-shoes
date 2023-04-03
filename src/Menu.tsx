decf7194622e17b9a4162eb4b71cfa24daa3d7ab
Cory House <housecor@gmail.com>
2023-04-03 15:43:41 -0500

    add reusable card

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,13 +1,14 @@
 import { Food, foods } from "./foods";
+import { Card } from "./reusable/Card";
 
 export function Menu() {
   function renderFood(food: Food) {
     return (
-      <div className="max-w-sm p-4 m-2 transition-colors bg-blue-100 border border-gray-400 rounded-lg shadow-lg hover:bg-indigo-600 hover:text-white">
+      <Card>
         <h2 className="text-2xl font-bold">{food.name}</h2>
         <p>{food.description}</p>
         <strong>${food.price}</strong>
-      </div>
+      </Card>
     );
   }
 
---
+++ src/reusable/Card.tsx
@@ -0,0 +1,11 @@
+type CardProps = {
+  children: React.ReactNode;
+};
+
+export function Card(props: CardProps) {
+  return (
+    <div className="max-w-sm p-4 m-2 transition-colors bg-blue-100 border border-gray-400 rounded-lg shadow-lg hover:bg-indigo-600 hover:text-white">
+      {props.children}
+    </div>
+  );
+}
