4129fce6ddf3e2c9f38817141a6d61b4ae695418
Cory House <housecor@gmail.com>
2023-04-03 13:52:29 -0500

    Add tailwind

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -2,12 +2,17 @@ import { Food, foods } from "./foods";
 
 export function Menu() {
   function renderFood(food: Food) {
-    return <li>{food.name}</li>;
+    return (
+      <li>
+        {food.name} - ${food.price}
+      </li>
+    );
   }
 
   return (
     <>
       <h1>Menu</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ul>{foods.map(renderFood)}</ul>
     </>
   );
