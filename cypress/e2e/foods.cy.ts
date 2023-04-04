2cad35562cde967c8aa245e85922e979af5fb92c
Cory House <housecor@gmail.com>
2023-04-04 15:46:41 -0500

    add

--- cypress/e2e/foods.cy.ts
+++ cypress/e2e/foods.cy.ts
@@ -6,3 +6,11 @@ it("should display the foods page at the root URL and support filtering menu ite
   cy.findByLabelText("Filter by tag").select("Breakfast");
   cy.findByText("1 Breakfast items found.");
 });
+
+it("should support navigating between pages", () => {
+  cy.visit("http://localhost:3000");
+  cy.findByRole("link", { name: "Manage Menu" }).click();
+  cy.findByRole("heading", { name: "Manage Menu" });
+  cy.findByRole("link", { name: "Menu" }).click();
+  cy.findByRole("heading", { name: "Menu" });
+});
