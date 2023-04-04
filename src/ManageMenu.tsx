91a31f185c1d1e149fcd5fb69e4ea4bd035167cc
Cory House <housecor@gmail.com>
2023-04-04 16:09:26 -0500

    consume component

--- src/ManageMenu.tsx
+++ src/ManageMenu.tsx
@@ -1,17 +1,14 @@
+import { Input } from "./reusable/Input";
+
 export default function ManageMenu() {
   return (
     <>
       <h1>Manage Menu</h1>
 
       <form>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" className="border" />
-
-        <label htmlFor="description">Description</label>
-        <input id="description" type="text" className="border" />
-
-        <label htmlFor="price">Price</label>
-        <input id="price" type="text" className="border" />
+        <Input id="name" label="Name" />
+        <Input id="description" label="Description" />
+        <Input id="price" label="Price" />
       </form>
     </>
   );
