e3a0d417f905656f3ceae1b04dc6a59a645b6f5a
Cory House <housecor@gmail.com>
2023-04-04 13:57:42 -0500

    add test

--- cypress/e2e/foods.cy.ts
+++ cypress/e2e/foods.cy.ts
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
-it("should display the foods page at the root URL", () => {
+it("should display the foods page at the root URL and support filtering menu items", () => {
   cy.visit("http://localhost:3000");
+  cy.findByRole("heading", { name: "Menu" });
+  cy.findByLabelText("Filter by tag").select("Breakfast");
+  cy.findByText("1 Breakfast items found.");
 });
