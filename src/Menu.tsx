492beeb86870c625310e9fbfd7856155862dae7e
Cory House <housecor@gmail.com>
2023-04-05 16:37:24 -0500

    Implement spinner

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -3,15 +3,18 @@ import { Food, FoodTag, foodTags } from "./foods";
 import { Card } from "./reusable/Card";
 import { deleteFood, getFoods } from "./services/foods.service";
 import { Button } from "./reusable/Button";
+import { Spinner } from "./reusable/Spinner";
 
 export function Menu() {
   const [tagFilter, setTagFilter] = useState<FoodTag | "">("");
   const [foods, setFoods] = useState<Food[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchFoods() {
       const foods = await getFoods();
       setFoods(foods);
+      setIsLoading(false);
     }
     fetchFoods();
     // Empty array means only run this once after the first render.
@@ -42,29 +45,37 @@ export function Menu() {
     );
   }
 
+  function renderMenu() {
+    return (
+      <>
+        <label htmlFor="tag-filter-select">Filter by tag</label>
+        <br />
+        <select
+          id="tag-filter-select"
+          value={tagFilter}
+          onChange={(event) => setTagFilter(event.target.value as FoodTag)}
+        >
+          <option value="">All items</option>
+          {foodTags.map((tag) => (
+            <option key={tag} value={tag}>
+              {tag}
+            </option>
+          ))}
+        </select>
+
+        {tagFilter && (
+          <p>{`${filteredFoods.length} ${tagFilter} items found.`}</p>
+        )}
+
+        <div className="flex flex-wrap">{filteredFoods.map(renderFood)}</div>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Menu</h1>
-      <label htmlFor="tag-filter-select">Filter by tag</label>
-      <br />
-      <select
-        id="tag-filter-select"
-        value={tagFilter}
-        onChange={(event) => setTagFilter(event.target.value as FoodTag)}
-      >
-        <option value="">All items</option>
-        {foodTags.map((tag) => (
-          <option key={tag} value={tag}>
-            {tag}
-          </option>
-        ))}
-      </select>
-
-      {tagFilter && (
-        <p>{`${filteredFoods.length} ${tagFilter} items found.`}</p>
-      )}
-
-      <div className="flex flex-wrap">{filteredFoods.map(renderFood)}</div>
+      {isLoading ? <Spinner /> : renderMenu()}
     </>
   );
 }
