c85882f3c45a17312b0c39ca6da4796a9ad5480d
Cory House <housecor@gmail.com>
2023-04-05 12:08:36 -0500

    enhance test

--- cypress/e2e/manageMenu.cy.ts
+++ cypress/e2e/manageMenu.cy.ts
@@ -4,8 +4,10 @@ it("should support adding a new menu item", () => {
   cy.visit("http://localhost:3000/manage");
   cy.findByLabelText("Name").type("New Menu Item");
   cy.findByLabelText("Description").type("Description");
-  cy.findByLabelText("Price").type("10");
+  cy.findByLabelText("Price").clear().type("10.95");
   cy.findByRole("button", { name: "Save Menu Item" }).click();
   cy.findByRole("status").should("have.text", "Menu Item Saved.");
   cy.findByRole("heading", { name: "New Menu Item" });
+  cy.findByText("Description");
+  cy.findByText("$10.95");
 });
