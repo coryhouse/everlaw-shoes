946c99a7bfb20c93ef6da6de354b6373060204b3
Cory House <housecor@gmail.com>
2023-04-06 17:02:05 -0500

    add rq

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -5,31 +5,22 @@ import { deleteFood, getFoods } from "./services/foods.service";
 import { Button } from "./reusable/Button";
 import { Spinner } from "./reusable/Spinner";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 export function Menu() {
   const [tagFilter, setTagFilter] = useState<FoodTag | "">("");
-  const [foods, setFoods] = useState<Food[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState("");
 
-  useEffect(() => {
-    async function fetchFoods() {
-      try {
-        const foods = await getFoods();
-        setFoods(foods);
-        setIsLoading(false);
-      } catch (error) {
-        setError("Fetch failed.");
-      }
-    }
-    fetchFoods();
-    // Empty array means only run this once after the first render.
-  }, []);
+  const getFoodsQuery = useQuery({
+    queryKey: ["foods"],
+    queryFn: getFoods,
+    useErrorBoundary: true,
+    initialData: [],
+  });
 
   // Derived state
   const filteredFoods = tagFilter
-    ? foods.filter((food) => food.tags.includes(tagFilter))
-    : foods;
+    ? getFoodsQuery.data.filter((food) => food.tags.includes(tagFilter))
+    : getFoodsQuery.data;
 
   function renderFood(food: Food) {
     return (
@@ -44,7 +35,7 @@ export function Menu() {
           onClick={() => {
             // Optimistic delete
             deleteFood(food.id);
-            setFoods(foods.filter((f) => f.id !== food.id));
+            // setFoods(foods.filter((f) => f.id !== food.id));
           }}
           type="button"
           aria-label={"Delete " + food.name}
@@ -82,13 +73,15 @@ export function Menu() {
     );
   }
 
-  // This will trigger the error boundary.
-  if (error) throw new Error(error);
-
   return (
     <>
+      {getFoodsQuery.isRefetching && <p>Checking for fresh data...</p>}
       <h1>Menu</h1>
-      {isLoading ? <Spinner /> : renderMenu()}
+      {
+        <Spinner isLoading={getFoodsQuery.data.length === 0}>
+          {renderMenu()}
+        </Spinner>
+      }
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,38 +6,45 @@ import ManageMenu from "./ManageMenu";
 import { Nav } from "./Nav";
 import { Toaster } from "react-hot-toast";
 import { ErrorBoundary } from "./reusable/ErrorBoundary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ErrorBoundary>
-    <BrowserRouter>
-      <Toaster />
-      <header>
-        Logo
-        <Nav />
-      </header>
-      <main>
-        <ErrorBoundary>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ErrorBoundary>
-                  <Menu />
-                </ErrorBoundary>
-              }
-            />
-            <Route
-              path="/manage/:id?"
-              element={
-                <ErrorBoundary>
-                  <ManageMenu />
-                </ErrorBoundary>
-              }
-            />
-          </Routes>
-        </ErrorBoundary>
-      </main>
-      <footer>Copyright 2023</footer>
-    </BrowserRouter>
-  </ErrorBoundary>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Toaster />
+        <header>
+          Logo
+          <Nav />
+        </header>
+        <main>
+          <ErrorBoundary>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ErrorBoundary>
+                    <Menu />
+                  </ErrorBoundary>
+                }
+              />
+              <Route
+                path="/manage/:id?"
+                element={
+                  <ErrorBoundary>
+                    <ManageMenu />
+                  </ErrorBoundary>
+                }
+              />
+            </Routes>
+          </ErrorBoundary>
+        </main>
+        <footer>Copyright 2023</footer>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </QueryClientProvider>
 );
